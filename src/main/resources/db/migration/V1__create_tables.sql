a3e204241c650dc2d10fcb22213c0f1196ea7e2e
Thiago Costa <thagocostanascimento@gmail.com>
2025-05-05 23:58:20 -0300

    Soft Delete Empresa e Ajuste no Repositório

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS candidates (
 
 CREATE TABLE IF NOT EXISTS company (
     id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -30,7 +31,7 @@ INSERT INTO admin_users (id, username, email, password, created_at)
 VALUES (
     '550e8400-e29b-41d4-a716-446655440000', 
     'admin', 
-    'admin@gestaovaga.com', 
-    '$2a$12$MRp/aMjxxba6X1ETJMmee.d/BYRLhRv38g7adG2ceVOE5SIOiTrTC', 
+    'admin@gestaovagasapp.com', 
+    '$2a$10$3Qrx0rv8qSmZ8s3RlD5qE.uSC1c9jO2RIkH5oPBMCjWk5kKJBxJMG', 
     CURRENT_TIMESTAMP
 ) ON CONFLICT DO NOTHING; 
