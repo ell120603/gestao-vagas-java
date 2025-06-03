73a0c2f92e37f31499784fab0fa94f94db9995ab
ell1206 <elielcamara2@gmail.com>
2025-06-03 10:39:48 -0300

    teste

--- src/main/java/br/com/eliel/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/eliel/gestao_vagas/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -34,21 +35,21 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                
-                .requestMatchers("/api/swagger-ui/**").permitAll()
-                .requestMatchers("/api/v3/api-docs/**").permitAll()
-                
-                
                 .requestMatchers("/api/company/**").permitAll()
                 .requestMatchers("/api/candidate/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/job/**").permitAll()
-                
                 
                 .anyRequest().authenticated()
             )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
         
         return http.build();
+    }
+    
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/api/swagger-ui/**", "/api/v3/api-docs/**");
     }
     
     @Bean
