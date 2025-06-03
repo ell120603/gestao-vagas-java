b96807b7361f369b098964124302d82509634804
ell1206 <elielcamara2@gmail.com>
2025-06-02 22:19:18 -0300

    config

--- src/main/java/br/com/eliel/gestao_vagas/config/SwaggerConfig.java
+++ src/main/java/br/com/eliel/gestao_vagas/config/SwaggerConfig.java
@@ -1,33 +1,26 @@
 package br.com.eliel.gestao_vagas.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("API de Gestão de Vagas")
-                        .description("API para gerenciamento de vagas de emprego, candidatos e empresas")
-                        .version("1.0.0"))
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new Components()
-                        .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
-    }
-
-    private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .bearerFormat("JWT")
-                .scheme("bearer");
+                        .version("1.0")
+                        .description("API para gerenciamento de vagas de emprego")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .schemaRequirement("bearer-key", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
     }
 }
