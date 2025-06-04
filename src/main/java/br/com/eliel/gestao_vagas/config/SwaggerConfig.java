e1cb6346904c1b093a5c913a334d54413d9b8fe1
ell1206 <elielcamara2@gmail.com>
2025-06-04 09:18:48 -0300

    mudanças no swaggerconfig

--- src/main/java/br/com/eliel/gestao_vagas/config/SwaggerConfig.java
+++ src/main/java/br/com/eliel/gestao_vagas/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package br.com.eliel.gestao_vagas.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,15 +14,21 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearer-key";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("API de Gestão de Vagas")
                         .version("1.0")
                         .description("API para gerenciamento de vagas de emprego")
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .schemaRequirement("bearer-key", new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT"));
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
